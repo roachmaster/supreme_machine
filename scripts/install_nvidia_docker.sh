2ec422d0a7e08b1135fb8acccfcbb498d1cd37c9
Leonardo rocha <leonardorocha@Leonardos-Air.lan>
2025-02-04 20:21:28 -0600

    updating version

--- scripts/install_nvidia_docker.sh
+++ scripts/install_nvidia_docker.sh
@@ -8,9 +8,16 @@ echo "----------------------------"
 echo "Installing NVIDIA Container Toolkit..."
 echo "----------------------------"
 
-distribution=$(. /etc/os-release; echo "$ID""$VERSION_ID")
-curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
-curl -s -L https://nvidia.github.io/nvidia-docker/"$distribution"/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+# Manually override distribution value to a supported version.
+# Change "ubuntu20.04" to the supported version for your system if needed.
+distribution="ubuntu20.04"
+
+# Download and install the NVIDIA GPG key into a keyring file.
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-docker.gpg
+
+# Add the NVIDIA Docker repository using the overridden distribution.
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/nvidia-docker.gpg] https://nvidia.github.io/nvidia-docker/$distribution/$(dpkg --print-architecture) /" | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+
 sudo apt update
 sudo apt install -y nvidia-docker2
 sudo systemctl restart docker
